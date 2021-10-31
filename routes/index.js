2a3c18f245dcc72bda5cfbe9e0571a99bd6111e0
hlkirkness <hlkirkness@gmail.com>
2021-10-31 17:30:44 -0400

    api routes added

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./api/user-routes');
+const thoughtRoutes = require('./api/thought-routes');
+ 
+router.use('/api/users', userRoutes);
+router.use('/api/thoughts', thoughtRoutes);
+router.use((req, res) => {
+    res.status(404).send('<h1> 404 Error!</h1>');
+})
+module.exports = router;
